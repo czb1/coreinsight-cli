7412c34d11bff2506df49977ee9d0fd83653f302
czb1 <51730183+czb1@users.noreply.github.com>
2026-09-22 20:25:15 +0800

    refactor: name reference auth server default

--- internal/cli/http.go
+++ internal/cli/http.go
@@ -16,6 +16,8 @@ import (
 	"time"
 )
 
+const defaultAuthServer = "https://omtool.rnd.huawei.com"
+
 type Runtime struct {
 	CoreInsightServer string
 	ChatServer        string
@@ -44,7 +46,7 @@ func defaultRuntime() (*Runtime, error) {
 	return &Runtime{
 		CoreInsightServer: strings.TrimRight(core, "/"),
 		ChatServer:        strings.TrimRight(envOr("COREINSIGHT_CHAT_SERVER", core+"/chat"), "/"),
-		AuthServer:        strings.TrimRight(envOr("COREINSIGHT_AUTH_SERVER", "https://omtool.rnd.huawei.com"), "/"),
+		AuthServer:        strings.TrimRight(envOr("COREINSIGHT_AUTH_SERVER", defaultAuthServer), "/"),
 		AICommunityServer: strings.TrimRight(envOr("COREINSIGHT_AI_COMMUNITY_SERVER", "https://aicommunity.coreai.rnd.huawei.com"), "/"),
 		CoreHarnessServer: strings.TrimRight(envOr("COREINSIGHT_CORE_HARNESS_SERVER", "http://coreharness.spec.rnd.huawei.com"), "/"),
 		Timeout:           timeout,
